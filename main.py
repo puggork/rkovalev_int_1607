dcee3357879540fe2355d80df9f9ac165f5617d9
puggork <pugfry89@gmail.com>
2025-03-25 19:55:46 +0100

    Add xlmr approach Add progress bar for long data input

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import argparse
 
 import pandas as pd
 
+from tqdm import tqdm
+
 import sklearn
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
@@ -13,23 +15,27 @@ from approaches.xlmr import get_formality_xlmr
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-a", "--approach", type=str, help="Approach to formality detection: \"flesch\" or \"xlmr\"")
+parser.add_argument("-nr", "--nrows", type=int, help="Number of rows to evaluate (random sample)")
 
 args = parser.parse_args()
 approach = args.approach
+nrows = 2000 if args.nrows is None else args.nrows
 
 
 
 def main():
-    print("Hello!")
-    
+    df = pd.read_csv("data/test.csv")
+    df = df.sample(n=nrows)
+
     if approach == "flesch":
-        df = pd.read_csv("data/test.csv")
         df["pred"] = df["sentence"].apply(lambda x: get_flesch_reading_ease(x))
         mae = mean_absolute_error(df["avg_score"], df["pred"])
         mse = mean_squared_error(df["avg_score"], df["pred"])
         print("For Flesch Reading Ease: Mean Absolute Error - {0}, Mean Squared Error - {1}".format(mae, mse))
     elif approach == "xlmr":
-        df["pred"] = df["sentence"].apply(lambda x: get_formality_xlmr(x))
+        tqdm.pandas()
+
+        df["pred"] = df["sentence"].progress_apply(lambda x: get_formality_xlmr(x))
         accuracy = accuracy_score(df["formal"], df["pred"])
         precision = precision_score(df["formal"], df["pred"])
         recall = recall_score(df["formal"], df["pred"])
